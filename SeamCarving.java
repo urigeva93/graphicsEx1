dd0aec7342413806c63133007ed59841500e969a
Uri Geva <urigeva93@gmail.com>
2017-04-03 17:03:31 +0300

    Add files via upload

--- SeamCarving.java
+++ SeamCarving.java
@@ -35,7 +35,7 @@ public class SeamCarving {
 							+ Math.abs(colorNeighbor.getGreen() - selfColor.getGreen())) / 3;
 				}
 				energyMat[i][j] = sumNeighbors / numOfNeighbors; // normalizing
-																	
+
 			}
 		}
 		return energyMat;
@@ -78,5 +78,88 @@ public class SeamCarving {
 
 	public static double getGrayFromRGB(Color c) {
 		return (c.getBlue() + c.getGreen() + c.getRed()) / 3;
+	}
+
+	public static double[][] transposeMat(double[][] mat) {
+		int h = mat.length;
+		int w = mat[1].length;
+		double[][] res = new double[w][h];
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				res[j][i] = mat[i][j];
+			}
+		}
+		return res;
+	}
+
+	public static int straightSeam(double[][] energyMat) {
+		int h = energyMat.length;
+		int w = energyMat[1].length;
+		int indexSeam = -1; // index for the seam colum
+		double minSum = Double.POSITIVE_INFINITY, tempSum = 0;
+		for (int j = 0; j < w; j++) {
+			for (int i = 0; i < h; i++) {
+				tempSum += energyMat[i][j];
+			}
+			if (tempSum < minSum) {
+				minSum = tempSum;
+				indexSeam = j;
+			}
+			tempSum = 0;
+		}
+		return indexSeam;
+	}
+
+	public static double[][] dynamicEnergyMat(double[][] energyMat) {
+		int h = energyMat.length;
+		int w = energyMat[1].length;
+		for (int i = 1; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if (j == 0) {
+					energyMat[i][j] = energyMat[i][j] + Math.min(energyMat[i - 1][j], energyMat[i - 1][j + 1]);
+				} else if (j == h - 1) {
+					energyMat[i][j] = energyMat[i][j] + Math.min(energyMat[i - 1][j], energyMat[i - 1][j - 1]);
+				} else {
+					energyMat[i][j] = energyMat[i][j]
+							+ Math.min(energyMat[i - 1][j - 1], Math.min(energyMat[i - 1][j], energyMat[i - 1][j + 1]));
+				}
+			}
+		}
+		return energyMat;
+	}
+
+	public static List<Pixel> pickPixelsSeam(double[][] dynamicMat) {
+		int h = dynamicMat.length;
+		int w = dynamicMat[1].length;
+		int minIndex = -1;
+		double min = Double.POSITIVE_INFINITY; // TODO check if num is less
+												// than POS_INFINITY
+		List<Pixel> pixelsList = new ArrayList<Pixel>();
+
+		// finding the minimum in the last line
+		for (int j = 0; j < w; j++) {
+			if (dynamicMat[h - 1][j] < min) {
+				min = dynamicMat[h - 1][j];
+				minIndex = j;
+			}
+		}
+		pixelsList.add(new Pixel(h - 1, minIndex));
+		int curr = minIndex;
+		for (int i = h - 1; i > 0; i--) {
+			double leftNeighbor = (curr == 0) ? Double.POSITIVE_INFINITY : dynamicMat[i - 1][curr - 1];
+			double rightNeighbor = (curr == w - 1) ? Double.POSITIVE_INFINITY : dynamicMat[i - 1][curr + 1];
+
+			double nextMinPixel = Math.min(leftNeighbor,
+					Math.min(dynamicMat[i - 1][curr], rightNeighbor));
+			
+			if (nextMinPixel == dynamicMat[i - 1][curr - 1]) 
+				curr --;
+			else if (nextMinPixel == dynamicMat[i - 1][curr + 1]) 
+				curr ++;
+			
+			pixelsList.add(new Pixel(i - 1, curr));	
+		}
+		return pixelsList;
+
 	}
 }
