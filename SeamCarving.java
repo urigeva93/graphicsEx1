4d51d9998bce3a965b0299f2dc3f23eee9d4c3e9
Uri Geva <urigeva93@gmail.com>
2017-04-08 17:10:56 +0300

    Add files via upload

--- SeamCarving.java
+++ SeamCarving.java
@@ -1,5 +1,5 @@
-
-import java.awt.Color;
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
@@ -10,153 +10,204 @@ import java.util.Iterator;
 import java.util.List;
 
 public class SeamCarving {
-	public static double[][] computeEnergy(BufferedImage img) {
-		int h = img.getHeight();
-		int w = img.getWidth();
-		double sumNeighbors = 0;
-		double numOfNeighbors;
-		Color selfColor;
-		double[][] energyMat = new double[h][w];
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
 
-				sumNeighbors = 0;
-				Pixel p = new Pixel(i, j);
-				List<Pixel> neighborsList = p.getNeighbors(w, h);
-				numOfNeighbors = neighborsList.size();
+    public static double[][] computeEnergy(BufferedImage img) {
+        int h = img.getHeight();
+        int w = img.getWidth();
+        double sumNeighbors;
+        double numOfNeighbors;
+        Color selfColor;
+        double[][] energyMat = new double[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
 
-				selfColor = new Color(img.getRGB(j, i));
-				while (neighborsList.size() > 0) {
+                sumNeighbors = 0;
+                Pixel p = new Pixel(i, j);
+                List<Pixel> neighborsList = p.getNeighbors(w, h);
+                numOfNeighbors = neighborsList.size();
 
-					Pixel neighbor = neighborsList.remove(0);
-					Color colorNeighbor = new Color(img.getRGB(neighbor.getY(), neighbor.getX()));
-					sumNeighbors += (Math.abs(colorNeighbor.getRed() - selfColor.getRed())
-							+ Math.abs(colorNeighbor.getBlue() - selfColor.getBlue())
-							+ Math.abs(colorNeighbor.getGreen() - selfColor.getGreen())) / 3;
-				}
-				energyMat[i][j] = sumNeighbors / numOfNeighbors; // normalizing
+                selfColor = new Color(img.getRGB(j, i));
+                while (neighborsList.size() > 0) {
 
-			}
-		}
-		return energyMat;
-	}
+                    Pixel neighbor = neighborsList.remove(0);
+                    Color colorNeighbor = new Color(img.getRGB(neighbor.getY(), neighbor.getX()));
+                    sumNeighbors += (Math.abs(colorNeighbor.getRed() - selfColor.getRed())
+                            + Math.abs(colorNeighbor.getBlue() - selfColor.getBlue())
+                            + Math.abs(colorNeighbor.getGreen() - selfColor.getGreen())) / 3;
+                }
+                energyMat[i][j] = sumNeighbors / numOfNeighbors; //normalizing
 
-	public static double[][] computeEntropy(BufferedImage img) {
-		int h = img.getHeight();
-		int w = img.getWidth();
+            }
+        }
+        return energyMat;
+    }
 
-		double[][] entropyMat = new double[h][w];
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				Pixel p = new Pixel(i, j);
-				List<Pixel> pixelsList = p.getEnthropyMembers(w, h);
-				int sumGrey = 0;
+    public static double[][] computeEntropy(BufferedImage img) {
+        int h = img.getHeight();
+        int w = img.getWidth();
 
-				Iterator<Pixel> it = pixelsList.iterator();
+        double[][] entropyMat = new double[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                Pixel p = new Pixel(i, j);
+                List<Pixel> pixelsList = p.getEnthropyMembers(w, h);
+                int sumGrey = 0;
 
-				while (it.hasNext()) {
-					Pixel pGrey = it.next();
-					Color colorNeighbor = new Color(img.getRGB(pGrey.getY(), pGrey.getX()));
-					sumGrey += getGrayFromRGB(colorNeighbor);
-				}
+                Iterator<Pixel> it = pixelsList.iterator();
 
-				it = pixelsList.iterator();
-				double H = 0;
-				while (it.hasNext()) {
-					Pixel neighbor = it.next();
-					Color colorNeighbor = new Color(img.getRGB(neighbor.getY(), neighbor.getX()));
+                while (it.hasNext()) {
+                    Pixel pGrey = it.next();
+                    Color colorNeighbor = new Color(img.getRGB(pGrey.getY(), pGrey.getX()));
+                    sumGrey += getGrayFromRGB(colorNeighbor);
+                }
 
-					double P_mn = getGrayFromRGB(colorNeighbor) / sumGrey;
+                it = pixelsList.iterator();
+                double H = 0;
+                while (it.hasNext()) {
+                    Pixel neighbor = it.next();
+                    Color colorNeighbor = new Color(img.getRGB(neighbor.getY(), neighbor.getX()));
 
-					H -= (P_mn * Math.log(P_mn));
-				}
-				entropyMat[i][j] = H;
-			}
-		}
-		return entropyMat;
-	}
+                    double P_mn = getGrayFromRGB(colorNeighbor) / sumGrey;
 
-	public static double getGrayFromRGB(Color c) {
-		return (c.getBlue() + c.getGreen() + c.getRed()) / 3;
-	}
+                    H -= (P_mn * Math.log(P_mn));
+                }
+                entropyMat[i][j] = H;
+            }
+        }
+        return entropyMat;
+    }
 
-	public static double[][] transposeMat(double[][] mat) {
-		int h = mat.length;
-		int w = mat[1].length;
-		double[][] res = new double[w][h];
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				res[j][i] = mat[i][j];
-			}
-		}
-		return res;
-	}
+    public static double getGrayFromRGB(Color c) {
+        return (c.getBlue() + c.getGreen() + c.getRed()) / 3;
+    }
 
-	public static int straightSeam(double[][] energyMat) {
-		int h = energyMat.length;
-		int w = energyMat[1].length;
-		int indexSeam = -1; // index for the seam colum
-		double minSum = Double.POSITIVE_INFINITY, tempSum = 0;
-		for (int j = 0; j < w; j++) {
-			for (int i = 0; i < h; i++) {
-				tempSum += energyMat[i][j];
-			}
-			if (tempSum < minSum) {
-				minSum = tempSum;
-				indexSeam = j;
-			}
-			tempSum = 0;
-		}
-		return indexSeam;
-	}
+    public static double[][] transposeMat(double[][] mat) {
+        int h = mat.length;
+        int w = mat[1].length;
+        double[][] res = new double[w][h];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                res[j][i] = mat[i][j];
+            }
+        }
+        return res;
+    }
 
-	public static double[][] dynamicEnergyMat(double[][] energyMat) {
-		int h = energyMat.length;
-		int w = energyMat[1].length;
-		for (int i = 1; i < h; i++) {
-			for (int j = 0; j < w; j++) {
+    public static int straightSeam(double[][] energyMat) {
+        int h = energyMat.length;
+        int w = energyMat[1].length;
+        int indexSeam = -1; // index for the seam colum
+        double minSum = Double.POSITIVE_INFINITY, tempSum = 0;
+        for (int j = 0; j < w; j++) {
+            for (int i = 0; i < h; i++)
+                tempSum += energyMat[i][j];
+            if (tempSum < minSum) {
+                minSum = tempSum;
+                indexSeam = j;
+            }
+            tempSum = 0;
+        }
+        return indexSeam;
+    }
 
-				double leftNeighbor = (j == 0) ? Double.POSITIVE_INFINITY : energyMat[i - 1][j - 1];
-				double rightNeighbor = (j == w - 1) ? Double.POSITIVE_INFINITY : energyMat[i - 1][j + 1];
+    public static double[][] dynamicEnergyMat(double[][] energyMat) {
+        int h = energyMat.length;
+        int w = energyMat[1].length;
+        for (int i = 1; i < h; i++) {
+            for (int j = 0; j < w; j++) {
 
-				energyMat[i][j] = energyMat[i][j]
-						+ Math.min(leftNeighbor, Math.min(energyMat[i - 1][j], rightNeighbor));
-			}
-		}
-		return energyMat;
+                double leftNeighbor = (j == 0) ? Double.POSITIVE_INFINITY : energyMat[i - 1][j - 1];
+                double rightNeighbor = (j == w - 1) ? Double.POSITIVE_INFINITY : energyMat[i - 1][j + 1];
 
-	}
+                energyMat[i][j] = energyMat[i][j]
+                        + Math.min(leftNeighbor, Math.min(energyMat[i - 1][j], rightNeighbor));
+            }
+        }
+        return energyMat;
 
-	public static List<Pixel> pickPixelsSeam(double[][] dynamicMat) {
-		int h = dynamicMat.length;
-		int w = dynamicMat[1].length;
-		int minIndex = -1;
-		double minLastRow = Double.POSITIVE_INFINITY; 
-		List<Pixel> pixelsList = new ArrayList<Pixel>();
+    }
 
-		// finding the minimum in the last line
-		for (int j = 0; j < w; j++) {
-			if (dynamicMat[h - 1][j] < minLastRow) {
-				minLastRow = dynamicMat[h - 1][j];
-				minIndex = j;
-			}
-		}
-		pixelsList.add(new Pixel(h - 1, minIndex));
-		int curr = minIndex;
-		for (int i = h - 1; i > 0; i--) {
-			double leftNeighbor = (curr == 0) ? Double.POSITIVE_INFINITY : dynamicMat[i - 1][curr - 1];
-			double rightNeighbor = (curr == w - 1) ? Double.POSITIVE_INFINITY : dynamicMat[i - 1][curr + 1];
+    public static List<Pixel> pickNextSeam(double[][] dynamicMat) {
+        int h = dynamicMat.length;
+        int w = dynamicMat[1].length;
+        int minIndex = -1;
+        double minLastRow = Double.POSITIVE_INFINITY;
+        List<Pixel> pixelsList = new ArrayList<>();
 
-			double nextMinPixel = Math.min(leftNeighbor, Math.min(dynamicMat[i - 1][curr], rightNeighbor));
+        // find the minimum in the last line
+        for (int j = 0; j < w; j++) {
+            if (dynamicMat[h - 1][j] < minLastRow) {
+                minLastRow = dynamicMat[h - 1][j];
+                minIndex = j;
+            }
+        }
+        pixelsList.add(new Pixel(h - 1, minIndex));
+        int curr = minIndex;
+        for (int i = h - 1; i > 0; i--) {
+            double leftNeighbor = (curr == 0) ? Double.POSITIVE_INFINITY : dynamicMat[i - 1][curr - 1];
+            double rightNeighbor = (curr == w - 1) ? Double.POSITIVE_INFINITY : dynamicMat[i - 1][curr + 1];
 
-			if (nextMinPixel == dynamicMat[i - 1][curr - 1])
-				curr--;
-			else if (nextMinPixel == dynamicMat[i - 1][curr + 1])
-				curr++;
+            double nextMinPixel = Math.min(leftNeighbor, Math.min(dynamicMat[i - 1][curr], rightNeighbor));
 
-			pixelsList.add(new Pixel(i - 1, curr));
-		}
-		return pixelsList;
+            if (nextMinPixel == dynamicMat[i - 1][curr - 1])
+                curr--;
+            else if (nextMinPixel == dynamicMat[i - 1][curr + 1])
+                curr++;
 
-	}
+            pixelsList.add(new Pixel(i - 1, curr));
+        }
+        return pixelsList;
+
+    }
+
+    public static double[][] computeEnergyFromImage(BufferedImage img, EnergyTypes type) {
+        int w = img.getWidth();
+        int h = img.getHeight();
+        double[][] combinedMat = new double[h][w];
+        double[][] energyMat = computeEnergy(img);
+
+        if (type == EnergyTypes.REGULAR)
+            return computeEnergy(img);
+        else {
+            if (type == EnergyTypes.ENTROPY) {
+                double[][] entropyMat = computeEntropy(img);
+                for (int i = 0; i < h; i++) {
+                    for (int j = 0; j < w; j++) {
+                        combinedMat[i][j] = energyMat[i][j] + 3 * entropyMat[i][j];
+                    }
+                }
+            } else {
+                // return forward;
+            }
+        }
+        return combinedMat;
+
+
+    }
+
+    public static void saveImage(String path, BufferedImage inputImg) {
+        try {
+            ImageIO.write(inputImg, "png", new File(path));
+        } catch (IOException e) {
+            System.out.println("Error in saving file: " + e.getMessage());
+        }
+    }
+
+    public static BufferedImage deepCopy(BufferedImage img) {
+        ColorModel cm = img.getColorModel();
+        boolean isAlpha = cm.isAlphaPremultiplied();
+        WritableRaster raster = img.copyData(null);
+        return new BufferedImage(cm, raster, isAlpha, null);
+
+    }
+
+    public enum EnergyTypes {
+        REGULAR, FORWARD, ENTROPY
+    }
+
+    // TODO removeSeamFromImage
+    // TODO addSeamFromImage
+    // TODO forward
+
+
 }
